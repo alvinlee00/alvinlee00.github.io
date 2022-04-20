9db161415107bbbcab7ffda95333eb27f9825bba
Alvin Lee <alvinlee225@gmail.com>
2022-04-20 22:40:40 +0200

    adding navbar

--- src/App.js
+++ src/App.js
@@ -1,16 +1,19 @@
 import React from 'react';
+import Navbar from "./components/Navbar/Navbar";
 import './App.css';
+
 
 
 function App() {
   return (
     
     <div>
-      <span class="title">
-        <span class="title-content">Portfolio</span>
-        <span class="title-content title-second">by alvin lee</span>
-        <span class="title-content title-third">an aspiring software engineer</span>
-        <span class="title-content title-fourth">work in progress</span>
+      <Navbar />
+      <span className="title">
+        <span className="title-content">Portfolio</span>
+        <span className="title-content title-second">by alvin lee</span>
+        <span className="title-content title-third">an aspiring software engineer</span>
+        <span className="title-content title-fourth">work in progress</span>
       </span>
     </div>
   );
---
+++ src/components/Navbar/MenuItems.js
@@ -0,0 +1,23 @@
+export const MenuItems = [
+    {
+        label: 'Home',
+        url: '#',
+        cName: 'nav-links'
+    },
+    {
+        label: 'About',
+        url: '#',
+        cName: 'nav-links'
+    },
+    {
+        label: 'Resume',
+        url: '#',
+        cName: 'nav-links'
+    },
+    {
+        label: 'Contact Me',
+        url: '#',
+        cName: 'nav-links'
+    },
+    
+]
