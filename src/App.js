f1f33f62476f4e25fef8a136662ff8262b933557
Alvin Lee <alvinlee225@gmail.com>
2022-04-20 20:01:40 +0200

    edits

--- src/App.js
+++ src/App.js
@@ -10,6 +10,7 @@ function App() {
         <span class="title-content">Portfolio</span>
         <span class="title-content title-second">by alvin lee</span>
         <span class="title-content title-third">an aspiring software engineer</span>
+        <span class="title-content title-fourth">work in progress</span>
       </h1>
     </div>
   );
