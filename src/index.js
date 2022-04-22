ea9b2987208a97de208f3b586a5614ebad889a99
Alvin Lee <alvinlee225@gmail.com>
2022-04-22 23:46:50 +0200

    trying to fix multipage html

--- src/index.js
+++ src/index.js
@@ -19,3 +19,14 @@ root.render(
 
 );
 
+const root2 = ReactDOM.createRoot(document.getElementById('root2'));
+root2.render(
+    <Router>
+        <Routes>
+            <Route path="/" element={<Home />}/>
+            <Route path="/About" element={<About />}/>
+            <Route path="/Contact" element={<Contact />}/>
+        </Routes>    
+    </Router>
+
+);
