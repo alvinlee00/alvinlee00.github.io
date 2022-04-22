66b0257cf3a77eee0e4356f7346d03c488cb544a
Alvin Lee <alvinlee225@gmail.com>
2022-04-22 23:40:53 +0200

    trying to fix multipage html

--- src/index.js
+++ src/index.js
@@ -1,15 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
-import { BrowserRouter } from 'react-router-dom';
+import Home from './Home';
+import About from './About';
+import Contact from './Contact';
+import { BrowserRouter as Router, Route, Routes} from 'react-router-dom';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-    <BrowserRouter>
-        <App />
-    </BrowserRouter>
+    <Router>
+        <Routes>
+            <Route path="/" element={<Home />}/>
+            <Route path="/About" element={<About />}/>
+            <Route path="/Contact" element={<Contact />}/>
+        </Routes>    
+    </Router>
 
 );
 
