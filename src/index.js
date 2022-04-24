0b49064ee7c864b599d106adf73c4b3654d37cf9
Alvin Lee <alvinlee225@gmail.com>
2022-04-24 20:08:44 +0200

    changed color

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import * as serviceWorker from "./serviceWorker";
 import Home from './components/Home';
 import About from './components/About';
 import Contact from './components/Contact';
@@ -22,5 +21,3 @@ root.render(
 );
 
 
-
-serviceWorker.unregister();
