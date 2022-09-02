f758513a315e8145671e62433c272cc700b1b0d4
Alvin Lee <alvinlee225@gmail.com>
2022-09-02 19:49:51 -0400

    adding blogs

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import Navbar from "./Navbar";
 import './App.css';
+import { Link } from 'react-router-dom';
 
 
 function Projects(){
@@ -11,7 +12,7 @@ function Projects(){
         <h1 className="title-content bold">My Projects!</h1>
         <br></br>
         <span className="title-content title-third">Bellow you can find some projects I have worked on:</span>
-        <span className="title-content title-fourth"></span>
+        <span className="title-content title-fourth">Access my <Link to='/Blogs' target='_self'>blogs</Link> here</span>
         <span className="title-content title-fifth"></span>
       </div>
 		</div>
