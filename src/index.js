de6a588dca4e365aa9cd1f12d81ed64eb759eaa7
Alvin Lee <alvinlee225@gmail.com>
2022-09-02 15:47:42 -0400

    relinking

--- src/index.js
+++ src/index.js
@@ -13,11 +13,11 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     <Router>
         <Routes>
-            <Route path="/website1" element={<Home />}/>
-            <Route path="/website1/About" element={<About />}/>
-            <Route path="/website1/Contact" element={<Contact />}/>
-            <Route path="/website1/Resume" element={<Resume />}/>
-            <Route path="/website1/Projects" element={<Projects />}/>
+            <Route path="" element={<Home />}/>
+            <Route path="/About" element={<About />}/>
+            <Route path="/Contact" element={<Contact />}/>
+            <Route path="/Resume" element={<Resume />}/>
+            <Route path="/Projects" element={<Projects />}/>
         </Routes>    
     </Router>
 );
