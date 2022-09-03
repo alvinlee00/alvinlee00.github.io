dfa1b06eb683b1758dab2fbbfb242129a95149ff
Alvin Lee <alvinlee225@gmail.com>
2022-09-02 20:11:26 -0400

    adding blogs page

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -12,7 +12,7 @@ function Projects(){
         <h1 className="title-content bold">My Projects!</h1>
         <br></br>
         <span className="title-content title-third">Bellow you can find some projects I have worked on:</span>
-        <span className="title-content title-fourth">Access my <Link to='/Blogs' target='_self'>blogs</Link> here</span>
+        <span className="title-content title-fourth">Access my <Link to='/Blogs' target='_self'>blogs</Link></span>
         <span className="title-content title-fifth"></span>
       </div>
 		</div>
